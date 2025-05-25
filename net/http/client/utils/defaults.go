631bde2e0967482a20c3e6187e12681fd8d24384
Igor Shishkin <me@teran.dev>
2025-05-25 20:47:12 +0300

    Add depdecation notice for http client package
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- net/http/client/utils/defaults.go
+++ net/http/client/utils/defaults.go
@@ -1,3 +1,11 @@
+// Package utils implements HTTP utilities to ease multiple HTTP operations
+//
+// Deprecated: since first library version was implemented long ago Go ecosystem
+// grew enough with more mature, tested and stable libraries. Please consider using
+// another library for your purpose.
+//
+// In favor of backward compatibility this code won't be removed but it's frozen
+// for adding new features. Only major issues will be fixed.
 package utils
 
 const (
